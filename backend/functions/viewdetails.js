85fa7735a2a39ea567f2dd8e9d3d4c256bdb63b0
divamjain1234 <86912946+divamjain1234@users.noreply.github.com>
2025-05-23 06:48:08 +0100

    Update viewdetails.js

--- backend/functions/viewdetails.js
+++ backend/functions/viewdetails.js
@@ -2,7 +2,6 @@ const jobmodel=require('../models/job')
 const viewdetails=async(req,res)=>{
     try {
         let id=req.params.id
-        console.log(id);
     let job=await jobmodel.findById(id)
     if(!job){
         return res.status(401).json('job not found')
