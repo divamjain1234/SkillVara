405e67ef2b4a6695728d07f47c48e22a9a8055a3
divamjain1234 <86912946+divamjain1234@users.noreply.github.com>
2025-05-28 04:53:52 +0100

    Update Profileservice.jsx

--- frontend/src/services/Profileservice.jsx
+++ frontend/src/services/Profileservice.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 export const createprofile=async (formData)=>{
    try {
-    let response=await axios.post(`${process.env.REACT_APP_API_URL}/user/createprofile`,formData, {
+    let response=await axios.post(`${import.meta.env.VITE_API_URL}/user/createprofile`,formData, {
       headers: {
         "Content-Type": "multipart/form-data", // Required for file upload
       },
